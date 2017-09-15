dd4b358400c121fa5240318196c383492c18ee91
Yen-Kuan Wu <c14006078@gmail.com>
2017-09-15 14:54:24 +0800

    tmp commit for reinstalling OS

--- include/kernel/mutex.h
+++ include/kernel/mutex.h
@@ -1,11 +1,45 @@
 #ifndef _KERNEL_MUTEX_H
 #define _KERNEL_MUTEX_H
 
+#include "kernel/bitmap.h"
 typedef _Atomic int atomic_s32;
 
-typedef struct {
-    atomic_s32 val;
+/**
+ *  pthread.h: typedef pthread_mutex_t uint32_t
+ *
+ *    ----------------------
+ *    |    30      | 1 | 1 |
+ *    ----------------------
+ *          A        B   C
+ *
+ *  Initial with `-1`
+ *
+ *  Bit format defined:
+ *
+ *      C: 0 = locked, 1 = unlocked
+ *      B: 0 = already create mutex obj in kernel
+ *         1 = contrary
+ *      A: 0xffffffff = unlocked
+ *         otherwise  = pointer of kernel_mutex_t
+ */
+
+typedef struct{
+    struct thread_info      *owner;
+    //unsigned long         owner_prio;
+    struct list_head        list;
+    struct bitmap_struct    prio_bmp;
+    //kernel_mutex_t        *next;
 } kernel_mutex_t;
+
+// 0 be true, -1 be false
+typedef struct {
+    atomic_s32              lock: 1;
+    atomic_s32              create: 1;
+    union {
+        kernel_mutex_t      *kmutex: 30;
+        struct thread_info  *thrd: 30;
+    };
+} user_mutex_t;
 
 int sys_pthread_mutex_lock(kernel_mutex_t *mutex);
 int sys_pthread_mutex_unlock(kernel_mutex_t *mutex);
--- kernel/mutex.c
+++ kernel/mutex.c
@@ -2,62 +2,146 @@
 #include <kernel/mutex.h>
 #include <kernel/sched.h>
 #include <kernel/thread.h>
+#include <kernel/slab.h> //malloc
 
 #include "linux/list.h"
+#include "bitmap.h"
 
 static LIST_HEAD(mutex_head);
 
+kernel_mutex_t *kernel_mutex_init(void)
+{
+    struct mutex *kmtx = (struct mutex *) malloc(sizeof(struct mutex)); //should align with bytes
+
+    INIT_BITMAP(kmtx.prio_bmp);
+    INIT_LIST_HEAD(kmtx->list);
+
+    struct sync_chain *ch = sync_chain_init();
+
+    return kmtx;
+}
+
+void kernel_mutex_destroy(kernel_mutex_t *kmtx)
+{
+    free(kmtx);
+}
+
+int thread_requeue(struct thread_info *thrd)
+{
+    int state = thrd->state;
+    struct bitmap_struct *bmp = NULL;
+
+    switch (state) {
+        case THREAD_STATE_ACTIVED:
+            bmp = sched_struct->active;
+            break;
+        case THREAD_STATE_EXPIRED:
+            bmp = sched_struct->expire;
+        default:
+    }
+    
+    if (bmp) {
+        thread_dequeue(thrd, bmp);
+        thread_enqueue(thrd, bmp);
+    }
+
+    return 0;
+}
+
 /* The thread owns the mutex on return. We also check the case when the lock
  * has been released between the test of the mutex and this syscall. */
-int sys_pthread_mutex_lock(kernel_mutex_t *mutex)
+int sys_pthread_mutex_lock(user_mutex_t *umtx)
 {
-    mutex->val++;
-    if (!mutex->val)
-        return 0;
+/*
+	mutex->val++;
+	if (!mutex->val)
+		return 0;
+	CURRENT_THREAD_INFO(curr_thread);
+	curr_thread->ti_private = mutex;
+	curr_thread->ti_state = THREAD_STATE_BLOCKED;
+	list_add_tail(&curr_thread->ti_q, &mutex_head);
+	sched_elect(SCHED_OPT_NONE);
+*/
+
+    kernel_mutex_t *kmtx;
+
+    //BUG_ON(umtx->lock == 0);
+    /* create kernel mutex if uncreated */
+    if (umtx->create == -1) {//Yet create
+        kmtx = kernel_mutex_init();
+        kmtx->owner = umtx->thrd << 2;
+        kmtx->owner_prio = (umtx->thrd << 2)->priority;
+        umtx->kmutex = kmtx >> 2;
+    else
+        kmtx = umtx->kmutex << 2;
+    thread_enqueue(curr_thread, &kmtx->prio_bmp);
+
     CURRENT_THREAD_INFO(curr_thread);
-    curr_thread->ti_private = mutex;
-    curr_thread->ti_state = THREAD_STATE_BLOCKED;
-    list_add_tail(&curr_thread->ti_q, &mutex_head);
+    struct thread_info *owner = kmtx->owner;
+    int state = owner->state;
+
+    /* priority inheritance */
+    int max_prio = find_first_bit(&kmtx->prio_bmp);
+    if (max_prio > curr_thread->priority) {
+        owner->priority = curr_thread->priority;
+        thread_requeue(owner);
+    }
+
+
+
+    /* create thread prio slots for mutex or even semaphore */
+    if (!curr_thread->pi_private) {
+        curr_thread->pi_private = (void *) malloc(sizeof(struct bitmap_struct));
+        INIT_BITMAP((struct bitmap_struct)curr_thread->pi_private);
+    }
+	bitmap_enqueue(kmtx->list, curr_thread->ti_private)
+	curr_thread->ti_state = THREAD_STATE_BLOCKED;
+
     sched_elect(SCHED_OPT_NONE);
 
-    return 0;
+	return 0;
 }
 
-static struct thread_info *find_first_blocking_thread(kernel_mutex_t *mutex)
+static struct thread_info *find_first_blocking_thread(kernel_mutex_t *kmtx)
 {
-    struct thread_info *thread;
+	/*struct thread_info *thread;
 
-    list_for_each_entry(thread, &mutex_head, ti_q)
-    {
-        if (thread->ti_private == mutex)
-            return thread;
-    }
+	list_for_each_entry(thread, &mutex_head, ti_q) {
+		if (thread->ti_private == mutex)
+			return thread;
+	}*/
 
-    return NULL;
+    int max_prio = find_first_bit(kmtx->prio_bmp);
+
+	return bitmap_first_entry(kmtx->prio_bmp, max_prio, struct thread_info, ti_q);
 }
 
-int sys_pthread_mutex_unlock(kernel_mutex_t *mutex)
+int sys_pthread_mutex_unlock(user_mutex_t *umtx)
 {
-    struct thread_info *waiter = NULL;
+/*
+	struct thread_info *waiter = NULL;
 
-    mutex->val--;
-    if (mutex->val >= 0) {
-        waiter = find_first_blocking_thread(mutex);
-        if (!waiter) {
-            printk("[mutex_unlock] No blocking threads for mutex=<%p>\n",
-                   mutex);
-            return -1;
-        }
-        list_del(&waiter->ti_q);
-        sched_enqueue(waiter);
-    }
-    CURRENT_THREAD_INFO(curr_thread);
-    if (curr_thread->ti_state == THREAD_STATE_BLOCKED) {
-        sched_elect(SCHED_OPT_NONE);
-    } else if (waiter && (curr_thread->ti_priority <= waiter->ti_priority)) {
-        sched_enqueue(curr_thread);
-        sched_elect(SCHED_OPT_NONE);
-    }
+	mutex->val--;
+	if (mutex->val >= 0) {
+		waiter = find_first_blocking_thread(mutex);
+		if (!waiter) {
+			printk("[mutex_unlock] No blocking threads for mutex=<%p>\n",
+			       mutex);
+			return -1;
+		}
+		list_del(&waiter->ti_q);
+		sched_enqueue(waiter);
+	}
+	CURRENT_THREAD_INFO(curr_thread);
+	if (curr_thread->ti_state == THREAD_STATE_BLOCKED) {
+		sched_elect(SCHED_OPT_NONE);
+	} else if (waiter && (curr_thread->ti_priority <= waiter->ti_priority)) {
+		sched_enqueue(curr_thread);
+		sched_elect(SCHED_OPT_NONE);
+*/
+    mtx
 
-    return 0;
+    sched_enqueue();
+
+	return 0;
 }
